185954c75c97a18f23422e396e0321492e040440
C3H3-AI <256468226+C3H3-AI@users.noreply.github.com>
2026-08-02 00:05:21 +0800

    chore: bump version to 6.5.4

--- custom_components/hacs_vision/const.py
+++ custom_components/hacs_vision/const.py
@@ -3,7 +3,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "hacs_vision"
 DOMAIN_HACS = "hacs"
-VERSION = "7.1.3"
+VERSION = "6.5.4"
 
 PANEL_TITLE = "HACS Vision"
 PANEL_ICON = "hacs:hacs"
